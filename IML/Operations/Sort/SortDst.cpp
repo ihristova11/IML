8a5ab9d53351a48453a6b086170ccb112dcbcd97
Irina Hristova <hristova119@gmail.com>
2021-01-02 18:43:39 +0200

    Fix bug in vector order

--- IML/Operations/Sort/SortDst.cpp
+++ IML/Operations/Sort/SortDst.cpp
@@ -5,14 +5,24 @@
 
 std::vector<double> SortDst::execute(OperationParam param)
 {
-    std::vector<double> res;
-    std::vector<double> args = param.getArguments();
-    std::set<double> s(args.begin(), args.end());
-    res.assign(s.begin(), s.end());
-    return res; // res may not be needed
+	std::vector<double> args = param.getArguments();
+	std::vector<double>::iterator r, w;
+	std::set<int> tmpset;
+
+	for (r = args.begin(), w = args.begin(); r != args.end(); ++r)
+	{
+		if (tmpset.insert(*r).second)
+		{
+			*w++ = *r;
+		}
+	}
+
+	args.erase(w, args.end());
+
+	return args;
 }
 
 std::string SortDst::toString()
 {
-    return Constants::RemoveDuplicatesCommandName;
+	return Constants::RemoveDuplicatesCommandName;
 }
